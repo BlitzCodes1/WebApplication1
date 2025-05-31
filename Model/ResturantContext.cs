446ab3a4f66d355900c80a3c7772865874f594a9
amer alaa <ameralaa1234234@gmail.com>
2025-05-31 19:37:50 +0300

    Add project files.

---
+++ Model/ResturantContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Model
+{
+    public class ResturantContext : DbContext
+    {
+        
+        public DbSet<User> Users { get; set; }
+
+        public ResturantContext(DbContextOptions options ) : base(options)
+        {
+            
+        }
+    }
+}
